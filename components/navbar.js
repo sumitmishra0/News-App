48f26fe1c77452cb2cbecfe3af3c1f23e44f8abf
SUMIT MISHRA <sumitmishra.sm04@gmail.com>
2022-02-12 03:41:44 +0530

    News App

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+function navbar() {
+    return `<div id="navbar">
+        
+    <h3><a href="home.html">NEWS APP</a></h3>
+    <h3><a href="home.html">Home</a></h3>
+   
+    <h3><a href="search.html">Search</a></h3>
+</div>`;
+}
+
+export default navbar
